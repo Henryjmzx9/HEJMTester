c17218dea49e52e4e142865fe2793d705a6e14b6
Henry <jm25002@esfe.agape.edu.sv>
2025-10-07 15:09:49 -0600

    Se mejoro la clase TemperatureConverter y se agregaron pruebas unitarias

---
+++ HEJM.Temperatura.UnitTestMSTest/TemperatureConverterTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HEJM.Temperatura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEJM.Temperatura.Tests
+{
+    [TestClass()]
+    public class TemperatureConverterTests
+    {
+        [TestMethod()]
+        public void CelsiusToFahrenheitTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FahrenheitToCelsiusTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
--- HEJM.Temperatura/TemperatureConverter.cs
+++ HEJM.Temperatura/TemperatureConverter.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace HEJM.Temperatura
 {
-    internal class TemperatureConverter
+    public class TemperatureConverter
     {
+        public double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        public double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
     }
 }
