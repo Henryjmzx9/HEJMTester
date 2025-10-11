15746b18af64d29e87bf1adf09cb382131382a18
Henry <jm25002@esfe.agape.edu.sv>
2025-10-11 11:56:54 -0600

    se crearon tambien los  tests para las cadenas de texto

---
+++ HEJM.StringManipulator.UnitTest.AppxUnit/StringManipulatorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEJM.StringManipulator.UnitTest.AppxUnit
+{
+    public class StringManipulatorTests
+    {
+        private readonly StringManipulator manipulator = new StringManipulator();
+
+        [Fact]
+        public void ReverseString_InputHola_ReturnsAloh()
+        {
+            string result = manipulator.ReverseString("Hola");
+            Assert.Equal("aloH", result);
+        }
+
+        [Fact]
+        public void ReverseString_EmptyString_ReturnsEmpty()
+        {
+            string result = manipulator.ReverseString("");
+            Assert.Equal("", result);
+        }
+
+        [Fact]
+        public void RemoveSpaces_InputWithSpaces_ReturnsWithoutSpaces()
+        {
+            string result = manipulator.RemoveSpaces("H o l a");
+            Assert.Equal("Hola", result);
+        }
+
+        [Fact]
+        public void RemoveSpaces_InputWithoutSpaces_ReturnsSame()
+        {
+            string result = manipulator.RemoveSpaces("Hola");
+            Assert.Equal("Hola", result);
+        }
+    }
+}
