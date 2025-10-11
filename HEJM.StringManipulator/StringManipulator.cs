78a41839fe22231a910c8e5da60efae90d5fbaa0
Henry <jm25002@esfe.agape.edu.sv>
2025-10-11 11:50:33 -0600

    Se creo el proyecto yla clase para la manipulacion de texto de tipo biblioteca de clase

---
+++ HEJM.StringManipulator/StringManipulator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEJM.StringManipulator
+{
+    public class StringManipulator
+    {
+        public string ReverseString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+
+        public string RemoveSpaces(string input)
+        {
+            return input.Replace(" ", "");
+        }
+    }
+}
